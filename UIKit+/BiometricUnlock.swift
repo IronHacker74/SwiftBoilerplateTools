f48e1e87cf201ee00ce6d4e3f5ac30ef8028c24f
Andrew Masters <armast97@gmail.com>
2023-11-03 18:16:35 -0700

    Added BiometricUnlock to new UIKit+ folder; updated README with format and UIKit+ section.

---
+++ UIKit+/BiometricUnlock.swift
@@ -0,0 +1,30 @@
+//
+//  BiometricUnlock.swift
+//  Created by Andrew Masters on 7/18/23.
+//
+
+import LocalAuthentication
+
+public final class BiometricUnlock {
+    public static func unlockWithAppleAuth(completion: @escaping (Bool,Error?) -> (Void)) {
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) else {
+            print(error?.localizedDescription ?? "Can't evaluate policy")
+            return
+        }
+        Task {
+            do {
+                try await context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Authenicate to access")
+                print("Succcessful authentication")
+                DispatchQueue.main.async {
+                    completion(true, nil)
+                }
+            } catch let error {
+                completion(false, error)
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
